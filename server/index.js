dac545294613ac71df5e3d214b016de1b8c26223
Adeeba Anaam <adeebaanaam01@gmail.com>
2025-11-04 20:27:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-frontend-name.onrender.com"
+    "https://sbitmern1anaam.onrender.com"
   ],
   credentials: true
 }));
